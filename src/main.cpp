f237f0a985c3d7f60e0f3c7df2e3773ff1c90bfe
Mom <martin.mozi@kistler.com>
2019-07-08 12:57:40 +0200

    fixed comment

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char** argv)
     auto iRestApi = libRestApi::createRestApiServer(8080, "/api");
     iRestApi->start([](libRestApi::HttpMethod method, const std::string & url, const std::string & request)->std::string
     {
-        // toto uz je automaticky rozvlaknene a ma to vlastny stack, mozno tu pridam id threadu, aby sa dali nejako nad tym indexovat objekty
-            return "ff";
+		// this is isolated in thread	
+        return "{\"someKey\": \"This is json encoded response\"}";
     });
     return 1;
 }
